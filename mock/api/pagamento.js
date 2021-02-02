927b2d2684bdb1152374687164764b0737e5caf9
Giuseppe Mammolo <gmammolo@gmail.com>
2021-02-02 12:11:21 +0100

    feat(Errori) migliorata la gestione degli errori

--- mock/api/pagamento.js
+++ mock/api/pagamento.js
@@ -1,30 +1,42 @@
 module.exports = function (app) {
+  const pagamentoResponse = {
+    id: 18,
+    valore: 1.0,
+    timestamp: '2021-02-02T10:03:53.343',
+    type: 'pagamento',
+    from: 21,
+    from_name: 'Prodotti Scolastici',
+    to: 20,
+    to_name: 'Sedie da Manager',
+  };
+
+  const outOfBudget = {
+    type: 'OUT_OF_BUDGET',
+    message: 'budget del cliente 21 insufficiente',
+    timestamp: 1612260277651,
+  };
 
   const errorSaldo = {
-    "timestamp": "2021-01-12T17:28:34.610+0000",
-    "status": 400,
-    "error": "Bad Request",
-    "message": "Fondi del cliente 1 insufficienti",
-    "path": "/api/pagamenti"
-  }
+    type: 'INSUFFICIENT_FUNDST',
+    message: 'Fondi del cliente 21 insufficienti',
+    timestamp: 1612260138275,
+  };
 
-
-  app.get("/api/pagamenti", (req, res, next) => {
-    res.json([]);
+  app.get('/api/pagamenti', (req, res, next) => {
+    res.json([pagamentoResponse]);
   });
 
-  app.get("/api/pagamenti/:id", (req, res, next) => {
-    res.json({});
-  });
+  // app.get('/api/pagamenti/:id', (req, res, next) => {
+  //   res.json(pagamentoResponse);
+  // });
 
-  app.post("/api/pagamenti", (req, res, next) => {
-    if(req.body.value > 40) {
-      return res.json(errorSaldo);
+  app.post('/api/pagamenti', (req, res, next) => {
+    if (req.body.value > 200) {
+      return res.status(400).json(errorSaldo);
+    } else if (req.body.value > 20) {
+      return res.status(400).json(outOfBudget);
+    } else {
+      return res.json(pagamentoResponse);
     }
-    res.json({});
   });
-
-
-
-
-}
+};
